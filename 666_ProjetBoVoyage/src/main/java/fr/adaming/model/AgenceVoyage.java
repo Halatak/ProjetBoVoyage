9afc72848ea9f059f41c513bc6077b0cac7133be
IN-BR-014 <IN-BR-014@IN-BR-014>
2019-03-27 12:06:35 +0100

    Allan: mineur Ajout modele AgenceVoyage

---
+++ 666_ProjetBoVoyage/src/main/java/fr/adaming/model/AgenceVoyage.java
@@ -0,0 +1,71 @@
+package fr.adaming.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="agences")
+public class AgenceVoyage {
+
+	//attributs
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id_ag")
+	private int id;
+	private String telephone;
+	private String nom;
+	
+	//constructeurs
+	public AgenceVoyage() {
+		super();
+	}
+
+	public AgenceVoyage(String telephone, String nom) {
+		super();
+		this.telephone = telephone;
+		this.nom = nom;
+	}
+
+	public AgenceVoyage(int id, String telephone, String nom) {
+		super();
+		this.id = id;
+		this.telephone = telephone;
+		this.nom = nom;
+	}
+
+	//getters et setters
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	@Override
+	public String toString() {
+		return "AgenceVoyage [id=" + id + ", telephone=" + telephone + ", nom=" + nom + "]";
+	}
+	
+	
+}
