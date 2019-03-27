a61b9a513734f25bcdbd7810f721e094fac163fe
IN-BR-014 <IN-BR-014@IN-BR-014>
2019-03-27 10:56:28 +0100

    Allan:mineur Ajoute modele personne(abstraite)

---
+++ 666_ProjetBoVoyage/src/main/java/fr/adaming/model/Personne.java
@@ -0,0 +1,112 @@
+package fr.adaming.model;
+
+import java.util.Date;
+
+public abstract class Personne {
+	
+	//attributs
+	private int id;
+	private String civilite;
+	private String nom;
+	private String prenom;
+	private String telephone;
+	private Date dateNaissance;
+	private String numCarteBanquaire;
+	
+	//constructeurs
+	public Personne() {
+		super();
+	}
+
+	public Personne(String civilite, String nom, String prenom, String telephone, Date dateNaissance,
+			String numCarteBanquaire) {
+		super();
+		this.civilite = civilite;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.telephone = telephone;
+		this.dateNaissance = dateNaissance;
+		this.numCarteBanquaire = numCarteBanquaire;
+	}
+
+	public Personne(int id, String civilite, String nom, String prenom, String telephone, Date dateNaissance,
+			String numCarteBanquaire) {
+		super();
+		this.id = id;
+		this.civilite = civilite;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.telephone = telephone;
+		this.dateNaissance = dateNaissance;
+		this.numCarteBanquaire = numCarteBanquaire;
+	}
+
+	//getters setters
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCivilite() {
+		return civilite;
+	}
+
+	public void setCivilite(String civilite) {
+		this.civilite = civilite;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public Date getDateNaissance() {
+		return dateNaissance;
+	}
+
+	public void setDateNaissance(Date dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+
+	public String getNumCarteBanquaire() {
+		return numCarteBanquaire;
+	}
+
+	public void setNumCarteBanquaire(String numCarteBanquaire) {
+		this.numCarteBanquaire = numCarteBanquaire;
+	}
+
+	@Override
+	public String toString() {
+		return "Personne [id=" + id + ", civilite=" + civilite + ", nom=" + nom + ", prenom=" + prenom + ", telephone="
+				+ telephone + ", dateNaissance=" + dateNaissance + ", numCarteBanquaire=" + numCarteBanquaire + "]";
+	}
+	
+	
+	
+	
+	
+
+}
