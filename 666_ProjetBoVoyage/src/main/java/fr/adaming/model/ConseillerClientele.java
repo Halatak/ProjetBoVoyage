1c9b18f545322883c2409a91cf4ad379d5f68783
IN-BR-008 <IN-BR-008@IN-BR-008>
2019-03-27 11:21:17 +0100

    Conseiller clientele modele

--- 666_ProjetBoVoyage/src/main/java/fr/adaming/model/ConseillerClientele.java
+++ 666_ProjetBoVoyage/src/main/java/fr/adaming/model/ConseillerClientele.java
@@ -1,7 +1,6 @@
 package fr.adaming.model;
 
 import java.io.Serializable;
-import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -11,71 +10,56 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="clients")
-public class Client extends Personne implements Serializable{
+@Table(name="conseillerClienteles")
+public class ConseillerClientele extends Personne implements Serializable{
 	
-	//Declaration des attributs
-	
+	//declaration des attributs
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column(name="id_cl")
-	private int idCl;
+	@Column(name="id_con")
+	private int idCon;
 	private String mail;
 	private String mdp;
 
-	
 	//Transformation de l'association UML en JAVA
 	
-	
-	
 	//constructeur
+
+	public ConseillerClientele() {
+		super();
+	}
+	public ConseillerClientele(String mail, String mdp) {
+		super();
+		this.mail = mail;
+		this.mdp = mdp;
+	}
+	public ConseillerClientele(int idCon, String mail, String mdp) {
+		super();
+		this.idCon = idCon;
+		this.mail = mail;
+		this.mdp = mdp;
+	}
+
 	
-	public Client() {
-		super();
-	}
-
-	public Client(String mail, String mdp) {
-		super();
-		this.mail = mail;
-		this.mdp = mdp;
-	}
-
-	public Client(int idCl, String mail, String mdp) {
-		super();
-		this.idCl = idCl;
-		this.mail = mail;
-		this.mdp = mdp;
-	}
-
 	//getter et setter
 	
-
+	public int getIdCon() {
+		return idCon;
+	}
+	public void setIdCon(int idCon) {
+		this.idCon = idCon;
+	}
 	public String getMail() {
 		return mail;
 	}
-
-
 	public void setMail(String mail) {
 		this.mail = mail;
 	}
-
-
 	public String getMdp() {
 		return mdp;
 	}
-
-
 	public void setMdp(String mdp) {
 		this.mdp = mdp;
 	}
-
-	public int getIdCl() {
-		return idCl;
-	}
-
-	public void setIdCl(int idCl) {
-		this.idCl = idCl;
-	}
 	
-
 }
