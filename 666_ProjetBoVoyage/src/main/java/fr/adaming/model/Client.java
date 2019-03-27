cc3a0d4219cf72dbf3edfece79a0b3541a0f704a
IN-BR-008 <IN-BR-008@IN-BR-008>
2019-03-27 11:09:23 +0100

    Client modele

--- 666_ProjetBoVoyage/src/main/java/fr/adaming/model/Client.java
+++ 666_ProjetBoVoyage/src/main/java/fr/adaming/model/Client.java
@@ -1,5 +1,66 @@
 package fr.adaming.model;
 
-public class Client {
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="clients")
+public class Client extends Personne implements Serializable{
+	
+	//Declaration des attributs
+	
+	private String mail;
+	private String mdp;
+
+	
+	//Transformation de l'association UML en JAVA
+	
+	
+	
+	//constructeur
+	
+	public Client() {
+		super();
+	}
+	
+	public Client(String civilite, String nom, String prenom, String telephone, Date dateNaissance,
+			String numCarteBanquaire, String mail, String mdp) {
+		super(civilite, nom, prenom, telephone, dateNaissance, numCarteBanquaire);
+		this.mail = mail;
+		this.mdp = mdp;
+	}
+
+	public Client(int id, String civilite, String nom, String prenom, String telephone, Date dateNaissance,
+			String numCarteBanquaire, String mail, String mdp) {
+		super(id, civilite, nom, prenom, telephone, dateNaissance, numCarteBanquaire);
+		this.mail = mail;
+		this.mdp = mdp;
+	}
+
+	//getter et setter
+	
+
+	public String getMail() {
+		return mail;
+	}
+
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+
+	public String getMdp() {
+		return mdp;
+	}
+
+
+	public void setMdp(String mdp) {
+		this.mdp = mdp;
+	}
+	
 
 }
