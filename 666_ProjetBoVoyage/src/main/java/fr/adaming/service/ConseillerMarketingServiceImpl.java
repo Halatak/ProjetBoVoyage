717a87c815da7a984f5acdf2c610dcc18a097e0b
IN-BR-004 <IN-BR-004@IN-BR-004>
2019-03-27 16:41:58 +0100

    Amiral: MAJOR
    Conseiller mrketing Service fait

---
+++ 666_ProjetBoVoyage/src/main/java/fr/adaming/service/ConseillerMarketingServiceImpl.java
@@ -0,0 +1,28 @@
+package fr.adaming.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import fr.adaming.dao.IConseillerMarkDao;
+import fr.adaming.model.ConseillerMarketing;
+
+@Service("conMarkService")
+@Transactional
+public class ConseillerMarketingServiceImpl implements IConseillerMarketingService{
+
+	//transformation de l'association UML en JAVA
+	@Autowired
+	private IConseillerMarkDao conMarkDao;
+	
+	//le setter pour l'injection de dependance
+	public void setConMarkDao(IConseillerMarkDao conMarkDao) {
+		this.conMarkDao = conMarkDao;
+	}
+
+	@Override
+	public ConseillerMarketing getConseillerMarkByMail(String mail) {
+		return conMarkDao.getConsMarkByMail(mail);
+	}
+
+}
---
+++ 666_ProjetBoVoyage/src/main/java/fr/adaming/service/IConseillerMarketingService.java
@@ -0,0 +1,9 @@
+package fr.adaming.service;
+
+import fr.adaming.model.ConseillerMarketing;
+
+public interface IConseillerMarketingService {
+
+	public ConseillerMarketing getConseillerMarkByMail(String mail);
+	
+}
