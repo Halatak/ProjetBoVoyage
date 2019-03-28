39df7828c4fe28e244825091c34e0a64b29a4a42
IN-BR-004 <IN-BR-004@IN-BR-004>
2019-03-28 09:44:18 +0100

    Amiral: MAJOR
    Ajout de la navBar, de la page index.jsp, du dossier page, du controller
    voyage (incomplet)

---
+++ 666_ProjetBoVoyage/src/main/java/fr/adaming/controller/VoyageController.java
@@ -0,0 +1,42 @@
+package fr.adaming.controller;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/voyage")
+public class VoyageController {
+
+	//Methode pour convertir et afficher correctement les dates
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		//l'objet WebDataBinder sert à faire le lien entre les params de la requete et les objets java
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+
+		df.setLenient(false);
+
+		//la methode registerCustomEditor: à configurer la conversion du param recu au type de l'attribut
+
+		//l'objet CustomDateEditor: sert à lier la date recue comme params de la requete à l'attribut de l'objet etudiant
+
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(df, false));
+	}
+
+	//page d'accueil qui affiche la liste des voyages
+	@RequestMapping(value="/liste", method=RequestMethod.GET)
+	public ModelAndView afficheListe() {
+		return new ModelAndView("accueil", "liste", new ArrayList<>());
+	}
+
+}
