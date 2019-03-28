089afb54ac5eccfa8dd748e3ebfb56cf196dc39d
IN-BR-004 <IN-BR-004@IN-BR-004>
2019-03-28 14:55:24 +0100

    Amiral: minor
    ajout interface cartebancaire et accompagnant

--- 666_ProjetBoVoyage/src/main/java/fr/adaming/service/IAccompagnantService.java
+++ 666_ProjetBoVoyage/src/main/java/fr/adaming/service/IAccompagnantService.java
@@ -1,5 +1,21 @@
 package fr.adaming.service;
 
+import java.util.List;
+
+import fr.adaming.model.Accompagnant;
+
 public interface IAccompagnantService {
+	
+public List<Accompagnant> afficherListeAccompagnantService();
+	
+	public Accompagnant ajoutAccompagnantService(Accompagnant v);
+	
+	public void modifierAccompagnantService(Accompagnant v);
+	
+	public void supprAccompagnantService(Accompagnant v);
+	
+	public void supprAccompagnantByIdService (int id);
+	
+	public Accompagnant getAccompagnantByIdService (int id);
 
 }
