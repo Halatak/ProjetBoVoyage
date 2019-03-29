5f92843cee41e8334921d6473d7d9a33d152dbd2
IN-BR-004 <IN-BR-004@IN-BR-004>
2019-03-29 10:28:51 +0100

    Amiral: minor
    ajout des photos dans le dao de destination

--- 666_ProjetBoVoyage/src/main/java/fr/adaming/dao/IDestinationDao.java
+++ 666_ProjetBoVoyage/src/main/java/fr/adaming/dao/IDestinationDao.java
@@ -1,7 +1,13 @@
 package fr.adaming.dao;
 
+import java.util.List;
+
 import fr.adaming.model.Destination;
 
 public interface IDestinationDao extends IGeneriqueDao<Destination> {
+	
+	public List<Destination> getAllPhoto();
+	
+	public Destination getByIdPhoto(int id);
 
 }
