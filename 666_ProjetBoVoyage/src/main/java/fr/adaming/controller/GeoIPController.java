48a42ba04ad23eb2250be5bf9b8e6b595b9bff19
IN-BR-004 <IN-BR-004@IN-BR-004>
2019-03-31 16:45:14 +0200

    Amiral: minor
    Ajout de la librairie, modele et classe service pour la geolocalisation
    Pas du tout opérationnel

---
+++ 666_ProjetBoVoyage/src/main/java/fr/adaming/controller/GeoIPController.java
@@ -0,0 +1,31 @@
+package fr.adaming.controller;
+
+import java.io.IOException;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.adaming.model.GeoIP;
+import fr.adaming.service.RawDBGeoIPLocationService;
+
+@RestController
+public class GeoIPController {
+	
+	 private RawDBGeoIPLocationService locationService;
+     
+	    public GeoIPController() throws IOException {
+	        locationService = new RawDBGeoIPLocationService();
+	    }
+	     
+	    @RequestMapping("/GeoIPTest")
+	    public GeoIP getLocation(
+	      @RequestParam(value="ipAddress", required=true) String ipAddress
+	    ) throws Exception {
+	       
+	        GeoIPLocationService<String, GeoIP> locationService 
+	          = new RawDBGeoIPLocationService();
+	        return locationService.getLocation(ipAddress);
+	    }
+
+}
