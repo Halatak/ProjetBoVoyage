81ef725f113b225c3f5f9a7ea47c1cecf5a6d099
IN-BR-003 <IN-BR-003@IN-BR-003>
2019-04-03 14:25:47 +0200

    arthur : test géolocalisation

---
+++ 666_ProjetBoVoyage/src/main/java/fr/adaming/controller/MapController.java
@@ -0,0 +1,18 @@
+package fr.adaming.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MapController {
+
+	@RequestMapping(value = "/carte", method = RequestMethod.GET)
+    public String afficheCarte(HttpServletRequest request) {
+
+        return "testCarte";
+    }
+	
+}
